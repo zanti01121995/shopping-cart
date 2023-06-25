8394d84cc3b0fe30dc64c7c4ce1441a2437a9fef
Zanti01121995 <santhoshsekar788@gmail.com>
2023-06-25 14:27:50 +0530

    update

--- src/pricecart.jsx
+++ src/pricecart.jsx
@@ -7,18 +7,18 @@ function Cards(props){
         <div className="card mb-5 mb-lg-0">
           <div className="card-body">
             <h5 className="card-title text-muted text-uppercase text-center">{props.title}</h5>
-            <h6 className="card-price text-center">{props.price}<span class="period">{props.perMonth}</span></h6>
+            <h6 className="card-price text-center">{props.price}<span className="period">{props.perMonth}</span></h6>
             <hr />
             <ul className="fa-ul">
-              <li><span className="fa-li"><i class="fas fa-check"></i></span>{props.noOfUser}</li>
-              <li><span className="fa-li"><i class="fas fa-check"></i></span>{props.storageSize}</li>
-              <li><span className="fa-li"><i class="fas fa-check"></i></span>{props.noOfProjects}</li>
-              <li><span className="fa-li"><i class="fas fa-check"></i></span>{props.accessType}</li>
-              <li className="fa-li"><span class="fa-li"><i class="fas fa-times"></i></span>{props.project}</li>
-              <li className="fa-li"><span class="fa-li"><i class="fas fa-times"></i></span>{props.support}</li>
-              <li className="fa-li"><span class="fa-li"><i class="fas fa-times"></i></span>{props.domain}
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.noOfUser}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.storageSize}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.noOfProjects}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.accessType}</li>
+              <li className="fa-li"><span className="fa-li"><i className="fas fa-times"></i></span>{props.project}</li>
+              <li className="fa-li"><span className="fa-li"><i className="fas fa-times"></i></span>{props.support}</li>
+              <li className="fa-li"><span className="fa-li"><i className="fas fa-times"></i></span>{props.domain}
               </li>
-              <li className="fa-li"><span class="fa-li"><i class="fas fa-times"></i></span>{props.status}</li>
+              <li className="fa-li"><span className="fa-li"><i className="fas fa-times"></i></span>{props.status}</li>
             </ul>
             <div className="d-grid">
               <a href="#" className="btn btn-primary text-uppercase">Button</a>
